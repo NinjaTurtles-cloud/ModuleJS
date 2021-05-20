bf8ae3f923e469b837f0c2cb40763f44198ea8c5
NinjaTurtles-cloud <wilfried.delgrange1987@gmail.com>
2021-05-20 17:53:54 +0200

    ajout de keyboard.js

---
+++ keyboardJs/script.js
@@ -0,0 +1,37 @@
+const piano = document.querySelector('#piano');
+const data = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+
+let html =''
+for (let octave=0; octave<2; octave++){
+    for(let i=0; i<data.length; i++){
+        let hasSharp = (data[i]!='E' && data[i]!='B') ? true : false
+        html+=`<div class="whiteNote" data-code='${data[i]}${octave+4}'>`
+        if(hasSharp){
+            html+=`<div class='blackNote' data-code='${data[i]}#${octave+4}'></div>`
+        }
+        html+=`</div>`
+    }
+}
+piano.insertAdjacentHTML('beforeend', html)
+
+const notes = document.querySelectorAll('.whiteNote, .blackNote')
+const keys = ["q", "z", "s", "e", "d", "f", "t", "g", "y", "h", "u", "j", "k", "i", "l", "o", "m", "p", "ù", "w", "=", "]", "Backspace", "\\"]
+//const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+
+document.addEventListener('keydown',(e)=>{
+    e.preventDefault()
+    keys.forEach((key,index)=>{
+        if(e.key==key){
+            notes[index].style.background = (notes[index].classList.contains('whiteNote')) ? "#ccc" : "black"
+            synth.triggerAttackRelease(notes[index].dataset.code,'16n')
+        }
+    })
+})
+
+document.addEventListener('keyup',(e)=>{
+    keys.forEach((key,index)=>{
+        if(e.key==key){
+            notes[index].style.background = (notes[index].classList.contains('whiteNote')) ? "white" : "#777"
+        }
+    })
+})
